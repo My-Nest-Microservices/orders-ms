fa79f586221648fc8422afab735769e8b6d0119c
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-09-21 17:48:20 +0200

    refactor: to microservice

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+
+  const logger = new Logger('OrdersMS-main');
+  await app.listen();
+  logger.log(`Orders microservice is running on: ${envs.port}`);
 }
 bootstrap();

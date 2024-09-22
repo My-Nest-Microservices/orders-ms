bd053e292e55b854d7893b8fa1a2c13ae229b817
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-09-22 09:03:19 +0200

    feat: create-order dto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,35 @@
-export class CreateOrderDto {}
+import { OrderStatus } from '@prisma/client';
+import {
+  IsBoolean,
+  IsDate,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+} from 'class-validator';
+import { OrderStatusList } from '../enum/order.enum';
+
+export class CreateOrderDto {
+  @IsNumber()
+  @IsPositive()
+  totalAmount: number;
+
+  @IsNumber()
+  @IsPositive()
+  totalItems: number;
+
+  @IsDate()
+  @IsOptional()
+  paidAt: Date;
+
+  @IsEnum(OrderStatusList, {
+    message: (args) =>
+      `${args.value} is not a valid status, possible status values: ${OrderStatusList}`,
+  })
+  @IsOptional()
+  status: OrderStatus = OrderStatus.PENDING;
+
+  @IsBoolean()
+  @IsOptional()
+  paid: boolean = false;
+}
